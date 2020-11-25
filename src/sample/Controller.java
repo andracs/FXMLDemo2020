090b92c863b640e10c5ea63a791ca7bcbdbd4cd5
András Ács <acsandras@gmail.com>
2020-11-25 14:35:42 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class Controller {
+
+    // Kobler XML'en med java
+    @FXML
+    TextArea textFelt2;
+
+
+    public void visNoget() {
+        // System.out.println("Knap er klikket.");
+        textFelt2.appendText("HUrra! Det virker!\n");
+    }
+
+}
